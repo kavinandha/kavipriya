7add590181be5ee84e565345e55976d2d7cce832
kavinandha <49307897+kavinandha@users.noreply.github.com>
2019-04-23 14:26:15 +0530

    Create prg12.py

---
+++ prg12.py
@@ -0,0 +1,11 @@
+num = int(input("enter a number:"))
+temp = num
+rev = 0
+while temp !=0;
+  rev=(rev *10)+(temp%10)
+  temp=temp//rev:
+if num==rev:
+  print("number is palindrome")
+else:
+  print("number isnot palindrome")
+  
