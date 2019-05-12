90ac444640195a1c573c5a041b1f378753ae6fb5
kavinandha <49307897+kavinandha@users.noreply.github.com>
2019-05-12 20:16:10 +0530

    prg71.py

---
+++ prg71.py
@@ -0,0 +1,13 @@
+def reverse(s):
+    return s[::-1]
+def is palindrome(s):
+    rev=reverse(s)
+ if (s==rev):
+     return true
+    return false
+  s="malayalam"
+ ans =is palindrome(S)
+ if ans==1:
+  print("yes")
+ else:
+  print("no")
