a160b21d49355465d65238c2f0dd23db28a3c66b
kavinandha <49307897+kavinandha@users.noreply.github.com>
2019-05-14 12:34:40 +0530

    prg82.py

---
+++ prg82.py
@@ -0,0 +1,6 @@
+n=int(input("enter the number:"))
+m=int(input("enter the number:"))
+print("\noriginal number:",x)
+print("formatted number with sign: "+"{:+.2f}".x);
+print("original number:",y)
+print("formatted number with sign:"+"{:+.2f}".y);
