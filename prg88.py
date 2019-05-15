6031b1c573a5bd80b876c190c6247cf9d20e707f
kavinandha <49307897+kavinandha@users.noreply.github.com>
2019-05-15 14:55:44 +0530

    Update prg88.py

--- prg88.py
+++ prg88.py
@@ -4,8 +4,8 @@ return n
 if(a>b):
 return god(a-b,b)
 return god(a,b-a)
-def 1cm(a,b):
+def lcm(a,b):
 return (a*b)/god (a,b)  
 a=int(input("a:"))
 b=int(input("b:"))
-print('LCM of',a,'and',a, 'is',1cm(a,b))
+print('LCM of',a,'and',a, 'is',lcm(a,b))
